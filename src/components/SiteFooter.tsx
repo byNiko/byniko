903f21523aedb605c3e4d37ef122f668e65e9a36
nick <nick@byniko.com>
2025-06-24 08:47:24 -0700

    update phpm

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,6 +1,6 @@
 export default function SiteFooter() {
   return (
-    <footer className="bg-linear-to-tr  bg-stone-800 text-white rounded-lg py-8 px-4 shadow-md mt-16">
+    <footer className="hidden bg-linear-to-tr  bg-stone-800 text-white rounded-lg py-8 px-4 shadow-md mt-16">
       <div className="container mx-auto max-w-2xl xl:max-w-3xl ">
         <div className="flex flex-col gap-4 py-8 text-center">
           <div className="flex flex-col gap-2 text-center">
